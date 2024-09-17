ad49a476386ac1a34d3c3627e6d813a5b3013d0d
TheHackerMD <gceoalex@gmail.com>
2024-09-17 16:43:18 -0700

    Create sus.py

---
+++ sus.py
@@ -0,0 +1,13 @@
+def fizzBuzz(num):
+    for x in range (0, num+1):
+        if x % 3 == 0 and x % 5 == 0:
+            print("FizzBuzz")
+        elif x % 3 == 0:
+            print("Fizz")
+        elif x % 5 == 0:
+            print("Buzz")
+        else:
+            print(x)
+
+
+fizzBuzz(100)
